b17b918ab46d7ca7c1c955f6ede74bb69606af0b
João Victor <victorbarboza@outlook.com>
2016-04-18 22:56:28 -0300

    Added AbsQueue

---
+++ project-queue/include/AbsQueue.h
@@ -0,0 +1,22 @@
+/**Classe interface fila (não pode ser instanciada!). */
+template < class Object >
+class AbsQueue {
+	public:
+		AbsQueue ( void ) { /* Empty */ } // Default constructor
+		virtual ~AbsQueue( void ) { /* Empty */ } // Default destructor
+
+		// Basic members
+		virtual void enqueue ( const Object & x ) = 0;
+
+		virtual Object dequeue ( void ) = 0;
+
+		virtual Object getFront ( void ) const = 0;
+
+		virtual bool isEmpty ( void ) const = 0;
+
+		virtual void makeEmpty ( void ) = 0;
+
+	private:
+		// Disable copy constructor
+		AbsQueue ( const AbsQueue & ) { /* Empty */ }
+};
